77dd8615d6a88e43635851371eb579068ccd8180
BigShuang <bigshuang2020@gmail.com>
2026-07-27 15:33:53 +0800

    v1.2 game2 done

--- game2/game.py
+++ game2/game.py
@@ -17,32 +17,36 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Sky Plane Battle v1.2")
     clock = pygame.time.Clock()
+    # 按照配置的时间间隔注册敌机生成事件。
     pygame.time.set_timer(ADD_ENEMY_EVENT, ADD_ENEMY_INTERVAL)
 
     player = Player()
-    enemies = []
+    # TODO 2.2：创建用于管理所有敌机的列表。
+    # enemies = []
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == ADD_ENEMY_EVENT:
-                enemies.append(Enemy())
+            # TODO 2.2：收到敌机生成事件时，创建一个 Enemy 并加入敌机列表。
+            # if event.type == ADD_ENEMY_EVENT:
+            #     do something
 
         keys = pygame.key.get_pressed()
         player.update(keys)
 
-        for enemy in enemies:
-            enemy.update()
-        enemies = [enemy for enemy in enemies if not enemy.is_out_of_screen()]
+        # TODO 2.2：逐一更新所有敌机的位置。
+
+        # TODO 2.2：过滤敌机列表，清理已经完全飞出屏幕底部的敌机。
+        # enemies = [enemy for enemy in enemies if ... ]
 
         screen.fill(BACKGROUND_COLOR)
-        for enemy in enemies:
-            enemy.draw(screen)
+        # TODO 2.2：逐一绘制敌机列表中的所有敌机。
+
+
         player.draw(screen)
         pygame.display.update()
         clock.tick(FPS)
 
     pygame.quit()
-
